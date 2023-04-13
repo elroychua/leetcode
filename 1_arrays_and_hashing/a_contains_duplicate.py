7f1a5a835ef50b020c5dc8eb91a438238c49a474
Elroy <elroymx@gmail.com>
2023-04-13 17:06:16 +0800

    finished contains duplicate and valid anagram

--- 1_arrays_and_hashing/a_contains_duplicate.py
+++ 1_arrays_and_hashing/a_contains_duplicate.py
@@ -1,5 +1,23 @@
-def contains_duplicate():
-    print(0)
+def contains_duplicate(nums):
+    # Time: O(n)^2 | Space: O(n)^2
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+    return False
+    # Time: O(n) | Space: O(n)
+    # set_nums = set()
+    # for n in nums:
+    #     if n in set_nums:
+    #         return True
+    #     set_nums.add(n)
+    # return False
+print("Test Cases for contains duplicate: ")
+case_1 = [1,2,3,1]
+case_2 = [1,2,3,4]
+case_3 = [1,1,1,3,3,4,3,2,4,2]
 
+print(contains_duplicate(case_1))
+print(contains_duplicate(case_2))
+print(contains_duplicate(case_3))
 
-contains_duplicate()
---
+++ 1_arrays_and_hashing/b_valid_anagram.py
@@ -0,0 +1,19 @@
+def isAnagram(s, t):
+        #Time: O(n) | Space: O(n)
+        if len(s) != len(t):
+            return False
+        ascii_s, ascii_t = 0, 0
+        for i in range(len(s)):
+            ascii_s += ord(s[i])
+        for j in range(len(t)):
+            ascii_t += ord(t[j])
+        if ascii_s == ascii_t:
+            return True
+        return False
+print("Test Cases for valid anagram: ")
+s_1 = "anagram"
+t_1 = "nagaram"
+s_2 = "rat"
+t_2 = "car"
+print(isAnagram(s_1, t_1))
+print(isAnagram(s_2, t_2))
